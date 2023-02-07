3d66bccad1a2f84478d9c5943527fcf00ba8f237
taemin <myloopmy@gmail.com>
2023-02-07 13:30:58 +0900

    4hours done vue.js
    3버전 메뉴얼 세팅 후 기본적인 문법 및 데이터바인딩

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p> {{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'Taemin Kim',
+      message: 'Welcome Vue Js!',
+      arr: [],
+      ob: {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.text-red {
+  color: red;
+}
+</style>
